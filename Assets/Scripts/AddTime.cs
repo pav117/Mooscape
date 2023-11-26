9e3d573955e5daf1be79a0c246383b34e1cf00cb
pav117 <PV222@live.mdx.ac.uk>
2023-11-26 14:50:55 +0000

    Events
    Some sort of event system tied to score and inventory

--- Assets/Scripts/AddTime.cs
+++ Assets/Scripts/AddTime.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AddTime : MonoBehaviour
 {
     public int score = 7;
+
+    GameObject[] InvItem;
+
+    void Update()
+    {
+        InvItem = GameObject.FindGameObjectsWithTag("InvItem");
+
+        if (score == 18)
+        {
+            if (InvItem.Length == 0)
+            {
+                SceneManager.LoadScene(7);
+            }
+            else if (InvItem.Length >= 1)
+            {
+                SceneManager.LoadScene(8);
+            }
+        }
+    }
 }
