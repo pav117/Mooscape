08865592a713871c4b72a2d8ea130ef06388a478
pav117 <PV222@live.mdx.ac.uk>
2023-11-27 17:57:28 +0000

    Cow Speak Updates and New Room
    Moo

--- Assets/Scripts/AddTime.cs
+++ Assets/Scripts/AddTime.cs
@@ -5,24 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class AddTime : MonoBehaviour
 {
-    public int score = 7;
+    public int score = 6;
 
     GameObject[] InvItem;
+
+    GameObject[] Awakener;
 
     void Update()
     {
         InvItem = GameObject.FindGameObjectsWithTag("InvItem");
 
-        if (score == 18)
+        Awakener = GameObject.FindGameObjectsWithTag("Awakener");
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        if (Awakener.Length == 0)
         {
-            if (InvItem.Length == 0)
+            if (score == 18)
             {
-                SceneManager.LoadScene(7);
+                if (InvItem.Length == 0)
+                {
+                    if (sceneName != "Farm")
+                    {
+                        SceneManager.LoadScene(7);
+                    }
+                }
+                else if (InvItem.Length >= 1)
+                {
+                    SceneManager.LoadScene(8);
+                }
             }
-            else if (InvItem.Length >= 1)
-            {
-                SceneManager.LoadScene(8);
-            }
+        }
+        else if (Awakener.Length == 1)
+        {
+            ;
         }
     }
 }
--- Assets/Scripts/CowSpeak.cs
+++ Assets/Scripts/CowSpeak.cs
@@ -7,18 +7,55 @@ public class CowSpeak : MonoBehaviour
     public GameObject MooznovS;
     public GameObject MooznovI;
 
+    public GameObject MoodsS;
+    public GameObject MoodsI;
+
+    public GameObject MooznovAS;
+
+    public GameObject MoodsAS;
+
+    GameObject[] Awakener;
+
     void Start()
     {
 
     }
 
-    public void whenButtonClicked()
+    void Update()
     {
-        StartCoroutine(MooZSpeak());
-        StartCoroutine(MooZIcon());
+       Awakener = GameObject.FindGameObjectsWithTag("Awakener"); 
     }
 
-    public IEnumerator MooZSpeak()
+    public void whenButtonClicked1()
+    {
+        if (Awakener.Length == 0)
+        {
+            StartCoroutine(MooZSpeak1());
+            StartCoroutine(MooZIcon());
+        }
+        else if (Awakener.Length >= 1)
+        {
+            StartCoroutine(MooZSpeak2());
+            StartCoroutine(MooZIcon());
+        }
+    }
+
+    public void whenButtonClicked2()
+    {
+        if (Awakener.Length == 0)
+        {
+            StartCoroutine(MooDSpeak1());
+            StartCoroutine(MooDIcon());
+        }
+        else if (Awakener.Length >= 1)
+        {
+            StartCoroutine(MooDSpeak2());
+            StartCoroutine(MooDIcon());
+        }
+    }
+
+
+    public IEnumerator MooZSpeak1()
     {
         MooznovS.SetActive(true);
         yield return new WaitForSecondsRealtime(3);
@@ -30,5 +67,33 @@ public class CowSpeak : MonoBehaviour
         MooznovI.SetActive(true);
         yield return new WaitForSecondsRealtime(3);
         MooznovI.SetActive(false);
+    }
+
+    public IEnumerator MooZSpeak2()
+    {
+        MooznovAS.SetActive(true);
+        yield return new WaitForSecondsRealtime(3);
+        MooznovAS.SetActive(false);
+    }
+
+    public IEnumerator MooDSpeak1()
+    {
+        MoodsS.SetActive(true);
+        yield return new WaitForSecondsRealtime(3);
+        MoodsS.SetActive(false);
+    }
+
+    public IEnumerator MooDIcon()
+    {
+        MoodsI.SetActive(true);
+        yield return new WaitForSecondsRealtime(3);
+        MoodsI.SetActive(false);
+    }
+
+    public IEnumerator MooDSpeak2()
+    {
+        MoodsAS.SetActive(true);
+        yield return new WaitForSecondsRealtime(3);
+        MoodsAS.SetActive(false);
     }
 }
